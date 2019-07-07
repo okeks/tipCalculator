c1126af1d37e16561c6e915b008853c32b96fb2e
Princewill <okechukwuprincewillw@gmail.com>
2019-07-07 20:16:40 +0100

    first

---
+++ myscript.js
@@ -0,0 +1,7 @@
+var billAmount = document.getElementById("bill").valueAsNumber;
+var percentTip = document.getElementById("percent").valueAsNumber;
+var tipAmount = document.getElementById("tip");
+var total = document.getElementById("total");
+
+function calculate() {
+ 
